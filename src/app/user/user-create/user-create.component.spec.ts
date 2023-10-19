280454ae8d9bf754338f62fe9b4e83c38cbcf377
LuisFGu <luisfg0487@gmail..com>
2023-10-18 21:22:30 -0500

    add style create usar

--- src/app/user/user-create/user-create.component.spec.ts
+++ src/app/user/user-create/user-create.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserCreateComponent } from './user-create.component';
 
+
 describe('UserCreateComponent', () => {
   let component: UserCreateComponent;
   let fixture: ComponentFixture<UserCreateComponent>;
